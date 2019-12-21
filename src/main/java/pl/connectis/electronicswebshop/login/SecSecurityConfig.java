454d8949323aba08871e5a3836b9932c7851791a
Dawid Widulinski <dawid.widulinski@gmail.com>
2019-12-21 15:16:29 +0100

    tworzenie klasy order

--- src/main/java/pl/connectis/electronicswebshop/login/SecSecurityConfig.java
+++ src/main/java/pl/connectis/electronicswebshop/login/SecSecurityConfig.java
@@ -1,10 +1,13 @@
 package pl.connectis.electronicswebshop.login;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -35,12 +38,12 @@ public class SecSecurityConfig extends WebSecurityConfigurerAdapter {
                 .loginProcessingUrl("/perform_login")
                 .defaultSuccessUrl("/homepage.html", true)
                 //.failureUrl("/login.html?error=true")
-                .failureHandler(authenticationFailureHandler())
+               // .failureHandler(authenticationFailureHandler())
                 .and()
                 .logout()
                 .logoutUrl("/perform_logout")
-                .deleteCookies("JSESSIONID")
-                .logoutSuccessHandler(logoutSuccessHandler());
+                .deleteCookies("JSESSIONID");
+                //.logoutSuccessHandler(logoutSuccessHandler());
     }
 
     @Bean
